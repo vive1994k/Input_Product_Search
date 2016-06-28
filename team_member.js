e44a21ba79ab3f019aeaa44c17f76043e6f56279
Vivek <wiwakey@Viveks-MacBook-Pro-2.local>
2016-06-28 10:24:32 +0530

    Update Teams Now working

--- team_member.js
+++ team_member.js
@@ -1,28 +1,28 @@
 "use strict";
 //Mobile Number validator
 var IsMobValid = function(mobNo){
-	var mobregex = /^[0]?[789]\d{9}$/;
-	var matchflag =false;
-	if(mob_regex.test(mobNo)){
-			matchflag = true;
-			break;
-	}
-	return matchflag;
+    var mobregex = /^[0]?[789]\d{9}$/;
+    var matchflag =false;
+    if(mob_regex.test(mobNo)){
+            matchflag = true;
+            break;
+    }
+    return matchflag;
 };
 //Name Validator
 var IsNameCorrect = function(name){
-	//var nameregex = /^[a-z]([-']?[a-z]+)*( [a-z]([-']?[a-z]+)*)+$/i;
-	var nameregex = /^[a-z ,.'-]+$/i;
-	if (name.length > 36){
-		alert("Name too big");
-		return false;
-	}
-	else if (nameregex.test(name)){
-		alert("Name should contain only alphanumeric characters and hyphen");
-		return false;
-	}
-	else
-		return true;
+    //var nameregex = /^[a-z]([-']?[a-z]+)*( [a-z]([-']?[a-z]+)*)+$/i;
+    var nameregex = /^[a-z ,.'-]+$/i;
+    if (name.length > 36){
+        alert("Name too big");
+        return false;
+    }
+    else if (nameregex.test(name)){
+        alert("Name should contain only alphanumeric characters and hyphen");
+        return false;
+    }
+    else
+        return true;
 };
 //Getting List of teams And Updating List of teams
 $(document).ready(function(){
@@ -48,37 +48,44 @@ $(document).ready(function(){
     });
 });
 
+
 //Function for Creating New Team
 var postTeam = function(){
-    var teamId = document.getElementById("teamId").value;
     var teamName = document.getElementById("teamName").value;
     var pointOfContact = document.getElementById("pointOfContact").value;
+    var JSONObject =  {
+        name: teamName
+        // pointOfContact:pointOfContact
+    }
     var SrEngMan = document.getElementById("SrEngMan").value;
+    if(SrEngMan) {
+        JSONObject['SEM'] = SrEngMan;
+    }
     var EngMan = document.getElementById("EngMan").value;
+    if(EngMan){
+        JSONObject['EM'] = EngMan;
+    }
     var teamLead = document.getElementById("teamLead").value;
+    if(teamLead){
+        JSONObject['TL'] = teamLead;
+    }
     var projectManager =  document.getElementById("projectManager").value;
-    if(IsNameCorrect(SrEngMan));
-    if(IsNameCorrect(EngMan));
-    if(IsNameCorrect(teamLead));
-    if(IsNameCorrect(projectManager));
-    var JSONObject = {"id": teamId,"name": teamName,"pointOfContact": pointOfContact,"SEM": SrEngMan,"EM": EngMan,"TL": teamLead,"PM": projectManager};
+    if(projectManager){
+        JSONObject['PM'] = projectManager;
+    }
+    console.log(JSONObject);
+    //console.log(JSON);
     var result = $.ajax({
-    	url:"",
-    	type:"post",
-    	data: JSON.stringify(JSONObject),
-    	contentType: 'json',
-    	headers: {
-                "Content-Type": 'application/json'
-        },
+        url:"http://139.59.3.93:8080/teams?key=b42b5ee6-385b-11e6-ac61-9e71128cae77",
+        type:"post",
+        data: JSONObject,
         success: function (data) {
-            debugger
             console.log(data);
         },
-       	error: function(error){
-        	debugger
+        error: function(xhr, error, data ){
+            console.log(xhr, error, data);
         }
     });
-    $('#teamId').val('');
     $('#teamName').val('');
     $('#pointOfContact').val('');
     $('#SrEngMan').val('');
@@ -94,22 +101,29 @@ var postProfile = function(){
     var mobileNo = document.getElementById("mobileNo").value;
     var Erole = document.getElementById("Erole").value;
     var empteamName = document.getElementById("empteamName").value;
+    var JSONObject = {
+        name : empName,
+        empId : empId,
+        email : email,
+        mobile : mobileNo,
+        role : Erole,
+        team : empteamName
+    }
     var Eremarks =  document.getElementById("Eremarks").value;
-    var JSONObject = {  "name": empName ,"empId": empId,"email": email,"mobile": mobileNo,"Erole": role,"team": empteamName,"aboutMe": Eremarks};
+    if(Eremarks){
+        JSONObject['aboutMe'] = Eremarks
+    }
     var result = $.ajax({
-    	url:"",
-    	type:"post",
-    	data: JSON.stringify(JSONObject),
-    	contentType: 'json',
-    	headers: {
-                "Content-Type": 'application/json'
-        },
+        url:"http://139.59.3.93:8080/employees?key=b42b5ee6-385b-11e6-ac61-9e71128cae77",
+        type:"post",
+        data: JSONObject,
         success: function (data) {
-            debugger
+            //debugger
             console.log(data);
         },
-       	error: function(error){
-        	debugger
+        error: function(xhr, error, data ){
+            // debugger
+            console.log(xhr, error, data);
         }
     });
     $('#empName').val('');
@@ -120,4 +134,3 @@ var postProfile = function(){
     $('#empteamName').val('');
     $('#Eremarks').val('');    
 };
-
